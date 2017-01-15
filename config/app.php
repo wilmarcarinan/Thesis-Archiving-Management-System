4032f48d7b44b8cffbf3cdf4e2487bfb1958f919
wilmarcarinan <wpcarinan@gmail.com>
2017-01-15 17:56:27 +0800

    Added QR Code Feature.

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
 
     ],
 
--- resources/views/file/search.blade.php
+++ resources/views/file/search.blade.php
@@ -33,7 +33,31 @@
 										{{ $file->FileTitle }}
 									</a>
 								</td>
-								<td>{{$file->FileDescription}}</td>
+								<td>
+									<!-- Button trigger modal -->
+									<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal">
+										View here..
+									</button>
+
+									<!-- Modal -->
+									<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+										<div class="modal-dialog" role="document">
+											<div class="modal-content">
+												<div class="modal-header">
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+													<h4 class="modal-title" id="myModalLabel">Abstract</h4>
+												</div>
+												<div class="modal-body">
+													{{ $file->FileDescription }}
+													<br>{!! QrCode::size(300)->generate('localhost:8000'.$file->FilePath); !!}
+												</div>
+												<div class="modal-footer">
+													<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+												</div>
+											</div>
+										</div>
+									</div>
+								</td>
 								<td>{{ $file->created_at }}</td>
 								@if(Auth::user()->Role == 'Admin')
 									<td></td>
