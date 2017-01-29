df5a9fc715add927f50b496bce8b4db25a49078f
wilmarcarinan <wpcarinan@gmail.com>
2017-01-30 06:21:44 +0800

    Adjust some styling.

--- resources/views/file/list.blade.php
+++ resources/views/file/list.blade.php
@@ -1,40 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2><span class="glyphicon glyphicon-list-alt"></span> List All</h2>
-    <div class="table-responsive">          
-        <table class="table">
-            <thead>
-              <tr>
-                <th><span class="glyphicon glyphicon-sort-by-order"></span></th>
-                <th>Title</th>
-                <th>Category</th>
-                <th>Author/s</th>
-                <th>Adviser</th>
-                <th>Date</th>
-                <th><span class="glyphicon glyphicon-eye-open"></span></th>
-                <th><span class="glyphicon glyphicon-star-empty"></span></th>
-              </tr>
-            </thead>
-            <tbody>
-                <?php $no=1; ?>
-                @foreach($files as $file)
-                <tr>
-                    <td>{{$no++}}</td>
-                    <td>{{$file->FileTitle}}</td>
-                    <td>{{$file->Category}}</td>
-                    <td>{{$file->Authors}}</td>
-                    <td>{{$file->Adviser}}</td>
-                    <td>{{$file->created_at}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <br />
+    @if(Auth::user()->Role == 'Admin')
+        <div class="container" style="margin-right: 150px">
+    @else
+        <div class="container">
+    @endif
+            <h2><span class="glyphicon glyphicon-list-alt"></span> List All</h2>
+            <div class="table-responsive">          
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th><span class="glyphicon glyphicon-sort-by-order"></span></th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Author/s</th>
+                        <th>Adviser</th>
+                        <th>Date</th>
+                        <th><span class="glyphicon glyphicon-eye-open"></span></th>
+                        <th><span class="glyphicon glyphicon-star-empty"></span></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no=1; ?>
+                        @foreach($files as $file)
+                        <tr>
+                            <td>{{$no++}}</td>
+                            <td>{{$file->FileTitle}}</td>
+                            <td>{{$file->Category}}</td>
+                            <td>{{$file->Authors}}</td>
+                            <td>{{$file->Adviser}}</td>
+                            <td>{{$file->created_at}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <br />
+                </div>
+                <center>
+                <button type="button" class="btn btn-info">Load more</button>
+            </center>
         </div>
-        <center>
-	    <button type="button" class="btn btn-info">Load more</button>
-    </center>
-</div>
 @endsection
