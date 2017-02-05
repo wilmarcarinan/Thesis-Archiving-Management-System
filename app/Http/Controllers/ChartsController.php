f26084bacff269bc7f4af1762aa816440d9790b7
lwrnc1228 <lwrncsmsn1228@gmail.com>
2017-02-05 23:12:23 +0800

    Charts Controller

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -12,11 +12,33 @@ class ChartsController extends Controller
     public function index()
     {
         $chart = Charts::create('area', 'highcharts')
-            ->title('Dashboard')
-            ->labels(['First', 'Second', 'Third'])
-            ->values([5,10,20])
-            ->dimensions(1000,500)
-            ->responsive(True);
-        return view('admin.charts', ['chart' => $chart]);
+        ->title('Views per day')
+        ->labels(['day1', 'day2', 'day3'])
+        ->elementLabel("Total")
+        ->values([5,10,20])
+        ->dimensions(780,350)
+        ->responsive(True);
+
+        $chart2 = Charts::create('bar', 'highcharts')
+        ->title('User login per day')
+        ->elementLabel("Total")
+        ->labels(['day1', 'day2', 'day3'])
+        ->values([5,10,20])
+        ->dimensions(780,350)
+        ->responsive(True);
+
+        $chart3 = Charts::create('line', 'highcharts')
+        ->title('Files uploaded per day')
+        ->elementLabel("Total")
+        ->labels(['day1', 'day2', 'day3'])
+        ->values([5,10,20])
+        ->dimensions(780,350)
+        ->responsive(True);
+
+        return view('admin.charts', [
+            'chart' => $chart, 
+            'chart2' => $chart2,
+            'chart3' => $chart3
+            ]);
     }
 }
