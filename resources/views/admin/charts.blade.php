d215c895770f0efd57854108ffcb66844f985fdb
lwrnc1228 <lwrncsmsn1228@gmail.com>
2017-02-05 23:14:38 +0800

    charts UI

--- resources/views/admin/charts.blade.php
+++ resources/views/admin/charts.blade.php
@@ -5,7 +5,20 @@
 @endsection
 
 @section('content')
-<center>
-    {!! $chart->render() !!}
+<center>	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 col-md-offset-2" style="padding: 10px 15px;">
+				{!! $chart->render() !!}
+			</div>
+			<div class="col-md-5" style="padding: 10px 20px;">
+				{!! $chart2->render() !!}
+			</div>
+			<div class="col-md-10 col-md-offset-2" style="padding: 10px 0px 10px;">
+				{!! $chart3->render() !!}
+			</div>
+		</div>
+	</div>
+
 </center>
 @endsection
