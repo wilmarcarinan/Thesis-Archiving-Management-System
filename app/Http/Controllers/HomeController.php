4b2a7209221b3e1e0e978b229959fe16e80b2e77
wilmarcarinan <wpcarinan@gmail.com>
2017-02-06 04:27:15 +0800

    Some updates.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,6 +55,16 @@ class HomeController extends Controller
             ->dimensions(780,350)
             ->responsive(True);
 
+            // $chart3 = Charts::multi('areaspline', 'highcharts')
+            // ->title('Burndown Chart')
+            // ->colors(['rgb(46,112,160)', 'rgb(192,65,62)'])
+            // ->labels(['Sprint 1', 'Sprint 2'])
+            // ->elementLabel('Total Story Points')
+            // ->dataset('Ideal Story Points', [11,32,58,74])
+            // ->dataset('Current Story Points Completed',[11,11,11,11])
+            // ->dimensions(780,350)
+            // ->responsive(True);
+
             return view('admin.charts', [
                 'chart' => $chart, 
                 'chart2' => $chart2,
