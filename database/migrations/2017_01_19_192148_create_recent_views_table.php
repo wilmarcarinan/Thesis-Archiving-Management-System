6190e950c08ad39a01c1f80f2e7ba40bcf89b5aa
wilmarcarinan <wpcarinan@gmail.com>
2017-02-15 19:08:16 +0800

    Updated DB

--- database/migrations/2017_01_19_192148_create_recent_views_table.php
+++ database/migrations/2017_01_19_192148_create_recent_views_table.php
@@ -14,32 +14,20 @@ class CreateRecentViewsTable extends Migration
     public function up()
     {
         Schema::create('recent_views', function (Blueprint $table) {
-            // $table->increments('id');
-            // $table->tinyInteger('recent_view');
-            // $table->integer('user_id')->unsigned();
+            $table->integer('user_id')->index();
+            $table->integer('file_id')->index();
+            $table->timestamps();
+
+            // $table->primary(['user_id','file_id']);
+            // $table->integer('user_id')->index();
             // $table->foreign('user_id')
             //       ->references('id')->on('users')
             //       ->onDelete('cascade');
-            // $table->integer('file_id')->unsigned();
+            // $table->integer('file_id')->index();
             // $table->foreign('file_id')
             //       ->references('id')->on('files')
             //       ->onDelete('cascade');
             // $table->timestamps();
-
-            // $table->integer('user_id')->index();
-            // $table->integer('file_id')->index();
-            // $table->timestamps();
-
-            $table->primary(['user_id','file_id']);
-            $table->integer('user_id')->index();
-            $table->foreign('user_id')
-                  ->references('id')->on('users')
-                  ->onDelete('cascade');
-            $table->integer('file_id')->index();
-            $table->foreign('file_id')
-                  ->references('id')->on('files')
-                  ->onDelete('cascade');
-            $table->timestamps();
         });
     }
 
